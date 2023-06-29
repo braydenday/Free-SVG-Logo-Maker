3edbaf9a079b2f7f3eecfdc08dec1c5c1eb4e6e7
Brayden Day <braydenday99@gmail.com>
2023-06-28 21:58:29 -0600

    finished async init function to check for valid user responses, generate a new svg based on responses, and print/write out the new svg file

--- index.js
+++ index.js
@@ -56,3 +56,61 @@ function writeToFile(fileName, data) {
         console.log("successfully created a logo.svg!");
     });
 }
+// async function to check for the valid user responses, generate a new svg based on responses, and print/write the new svg
+async function init() {
+    console.log("starting init");
+	var svgString = "";
+	var svgFile = "logo.svg";
+
+// wait for questions to be answered before prompting answers
+    const answers = await inquirer.prompt(questions);
+
+// if statement to check for user text input response is between 1-3 characters and error if otherwise
+	var userText = "";
+	if (answers.text.length > 0 && answers.text.length < 4) {
+		userText = answers.text;
+	} else {
+		console.log("only use 1-3 Characters");
+        return;
+	}
+	console.log("user text: [" + userText + "]");
+
+//use the text color from input response
+	fontColor = answers.tcolor;
+	console.log("text color: [" + fontColor + "]");
+//use the shape color from input response
+	shapeColor = answers.scolor;
+	console.log("shape color: [" + shapeColor + "]");
+//use the shape from input response
+	shapeType = answers.shape;
+	console.log("entered shape = [" + shapeType + "]");
+	
+//if statements to generate new shape and set color based on the input response
+	let userShape;
+	if (shapeType === "square" || shapeType === "square") {
+		userShape = new Square();
+		console.log("square");
+	}
+	else if (shapeType === "sircle" || shapeType === "circle") {
+		userShape = new Circle();
+		console.log("circle");
+	}
+	else if (shapeType === "triangle" || shapeType === "triangle") {
+		userShape = new Triangle();
+		console.log("triangle");
+	}
+	else {
+		console.log("wrong shape");
+	}
+	userShape.setColor(shapeColor);
+
+//generate a new svg with the new shape and color
+	var svg = new Svg();
+	svg.setTextElement(userText, fontColor);
+	svg.setShapeElement(userShape);
+	svgString = svg.render();
+	
+	console.log("shape:\n\n" + svgString);
+	writeToFile(svgFile, svgString); 
+}
+init()
