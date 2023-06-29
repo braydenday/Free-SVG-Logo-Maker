3cf71b9da366fca8c31e84a850e29e242f1c2475
Brayden Day <braydenday99@gmail.com>
2023-06-28 20:49:09 -0600

    added node modules , module export for shapes, and svg constructor

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Runs the application using imports from lib/
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes");
+
+// contructor with 3 methods for rendering and setting the text/shape elements in the SVG. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/version
+class Svg {
+    constructor() {
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="50" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape) {
+        this.shapeElement = shape.render()
+
+    }
+}
